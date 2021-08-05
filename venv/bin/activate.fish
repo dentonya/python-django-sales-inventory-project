cc128b7b2646173a3639197211ba3154d5ad8366
den_atonya <denatonya@gmail.com>
2021-08-05 13:03:56 +0300

    minor modifications

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/denatonya/Documents/inventory-management-master/venv"
+set -gx VIRTUAL_ENV "/home/denatonya/Documents/GitHub/python-django-sales-inventory-project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
